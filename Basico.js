82dc0fe7a0da8bada0170254721783003e9e93c3
gabmadera <gabmadera@gmail.com>
2019-11-19 19:30:54 +0100

    basicos done

---
+++ Basico.js
@@ -0,0 +1,6 @@
+var miNombreApellidos = "Gabriel Madera Castell";
+var miEdad;
+miedad = 28
+var dia1 = "Lunes", dia2 ="Martes", dia3 ="Miercoles", dia4 ="Jueves", dia5 ="Viernes", dia6 ="Sabado", dia7 ="Domingo";
+var gravedad = 9.81;
+var impar1 = 1, impar2 = 3, impar3 = 5;
